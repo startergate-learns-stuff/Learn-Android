03dde3dade5a139e2402a68201666819f78b8310
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-09-30 17:47:59 +0900

    9.30

--- chapter05/app/src/main/java/com/example/chapter05/MainActivity.java
+++ chapter05/app/src/main/java/com/example/chapter05/MainActivity.java
@@ -2,6 +2,52 @@ package com.example.chapter05;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ViewFlipper;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button prevBtn, nextBtn;
+    ViewFlipper vf;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_viewflipper);
+
+        prevBtn = findViewById(R.id.prevBtn);
+        nextBtn = findViewById(R.id.nextBtn);
+
+        vf = findViewById(R.id.viewFlipper);
+
+        // '<' 버튼 클릭에 대한 기능 정의
+        prevBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // vf.showPrevious();
+                vf.setFlipInterval(500);
+                vf.startFlipping();
+            }
+        });
+
+        // '>' 버튼 클릭에 대한 기능 정의
+        nextBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                vf.showNext();
+            }
+        });
+    }
+}
+
+
+
+/*
+package com.example.chapter05;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,3 +57,4 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_slidingdrawer);
     }
 }
+*/
