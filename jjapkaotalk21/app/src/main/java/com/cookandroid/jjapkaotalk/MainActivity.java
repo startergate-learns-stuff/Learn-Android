e94eb241227128c6873c94c83f6d9b3f250fc055
STARTERGATE <32726809+startergate@users.noreply.github.com>
2019-10-21 22:57:47 +0900

    10.21

--- jjapkaotalk21/app/src/main/java/com/cookandroid/jjapkaotalk/MainActivity.java
+++ jjapkaotalk21/app/src/main/java/com/cookandroid/jjapkaotalk/MainActivity.java
@@ -1,5 +1,7 @@
 package com.cookandroid.jjapkaotalk;
 
+import android.support.v7.app.AlertDialog;
+import android.content.DialogInterface;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.ContextMenu;
@@ -7,13 +9,14 @@ import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     LinearLayout friend;
-    TextView friendName, title;
+    TextView friendName, friendStatus, title;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,7 @@ public class MainActivity extends AppCompatActivity {
 
         friend = findViewById(R.id.friend);
         friendName = findViewById(R.id.friendName);
+        friendStatus = findViewById(R.id.friendStatus);
 
         title = findViewById(R.id.title);
 
@@ -57,11 +61,36 @@ public class MainActivity extends AppCompatActivity {
 
         switch (item.getItemId()) {
             case R.id.favoriteOption:
-                friendName.append("★");
+                //friendName.append("★");
+
+                // 커스텀한 대화 상자를 만들어보기 (xml 레이아웃 파일을 팝업으로 띄우기)
+                final View statusView = View.inflate(MainActivity.this, R.layout.activity_status, null);
+                AlertDialog.Builder changeDialog = new AlertDialog.Builder(MainActivity.this);
+                changeDialog.setView(statusView);
+                changeDialog.setNegativeButton("취소", null);
+                changeDialog.setPositiveButton("변경", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        EditText et = (EditText) statusView.findViewById(R.id.status);
+                        friendStatus.setText(et.getText().toString());
+                    }
+                });
+                changeDialog.show();
                 break;
             case R.id.deleteOption:
-                friend.setVisibility(View.GONE);
-                title.setText("친구가 1명도 없는 짭카오톡ㅠㅠ");
+                // 팝업창처럼 사용 가능한 '대화 상자'를 만들어보자
+                AlertDialog.Builder delDialog = new AlertDialog.Builder(MainActivity.this);
+                delDialog.setTitle("에에");
+                delDialog.setMessage("진짜 혼또로 김도영 손절 데스까?");
+                delDialog.setNegativeButton("이이에", null);
+                delDialog.setPositiveButton("하잇", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        friend.setVisibility(View.GONE);
+                        title.setText("친구가 1명도 없는 짭카오톡ㅠㅠ");
+                    }
+                });
+                delDialog.show();
                 break;
             default:
                 return false;
